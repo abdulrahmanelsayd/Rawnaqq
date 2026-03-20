d137aef0e4ad1a8e6004bd16ab5aa2d4cd703ff3
abdulrahmanelsayd <abdulrahmaanelsayd@gmail.com>
2026-03-20 20:46:00 +0200

    feat: Update product catalog with exact requested perfumes

--- src/data.js
+++ src/data.js
@@ -24,6 +24,70 @@ export const config = {
   products: [
     {
       id: 1,
+      name: "بلو دي شانيل",
+      description: "Bleu De Chanel - الحرية في أسمى معانيها.",
+      price: "1400 جنية",
+      size: "100 مل",
+      image: "/img1_.jpg"
+    },
+    {
+      id: 2,
+      name: "سوفاج",
+      description: "Sauvage - نداء البرية وقوة الطبيعة.",
+      price: "1400 جنية",
+      size: "100 مل",
+      image: "/img2_.jpg"
+    },
+    {
+      id: 3,
+      name: "بكرات روج",
+      description: "Baccarat Rouge - سحر العنبر والزهور في تناغم فريد.",
+      price: "1500 جنية",
+      size: "100 مل",
+      image: "/img3_.jpg"
+    },
+    {
+      id: 4,
+      name: "ايربا بورا",
+      description: "Erba Pura - مزيج رائع من الفواكه المنعشة.",
+      price: "1200 جنية",
+      size: "100 مل",
+      image: "/img4_.jpg"
+    },
+    {
+      id: 5,
+      name: "ايروس فيرزاتشي",
+      description: "Versace Eros - عطر الحب والشغف والجمال.",
+      price: "1200 جنية",
+      size: "100 مل",
+      image: "/img5_.jpg"
+    },
+    {
+      id: 6,
+      name: "انفكتس",
+      description: "Invictus - عطر القوة والانتصار للرجل العصري.",
+      price: "1150 جنية",
+      size: "100 مل",
+      image: "/img6_.jpg"
+    },
+    {
+      id: 7,
+      name: "استرونجر ويذ يو",
+      description: "Stronger With You - جاذبية لا تقاوم وحضور قوي.",
+      price: "1250 جنية",
+      size: "100 مل",
+      image: "/img7_.jpg"
+    },
+    {
+      id: 8,
+      name: "سلفر ماونتن",
+      description: "Silver Mountain - انتعاش الجبال والهواء النقي.",
+      price: "1200 جنية",
+      size: "100 مل",
+      image: "/img8_.jpg"
+    },
+    {
+      id: 9,
       name: "اكوا دي جو",
       description: "Acqua Di Gio - انتعاش البحر الأبيض المتوسط في زجاجة.",
       price: "1200 جنية",
@@ -31,124 +95,60 @@ export const config = {
       image: "/img1_.jpg"
     },
     {
-      id: 2,
-      name: "انفكتس",
-      description: "Invictus - عطر القوة والانتصار للرجل العصري.",
-      price: "1150 جنية",
+      id: 10,
+      name: "كيركي",
+      description: "Kirke - عطر ساحر يجسد الفخامة والأناقة.",
+      price: "1200 جنية",
       size: "100 مل",
       image: "/img2_.jpg"
     },
     {
-      id: 3,
-      name: "الثائر",
-      description: "Al Thaer - عبق الفخامة والتميز.",
-      price: "1300 جنية",
+      id: 11,
+      name: "خمره",
+      description: "Khamrah - نفحات دافئة من التوابل والأخشاب الفاخرة.",
+      price: "1200 جنية",
       size: "100 مل",
       image: "/img3_.jpg"
     },
     {
-      id: 4,
-      name: "فويج",
-      description: "Voyage - رحلة عطرية تأخذك إلى عالم آخر.",
-      price: "1100 جنية",
+      id: 12,
+      name: "خمره قهوة",
+      description: "Khamrah Coffee - مزيج فريد من عبق القهوة والتوابل الدافئة.",
+      price: "1200 جنية",
       size: "100 مل",
       image: "/img4_.jpg"
     },
     {
-      id: 5,
-      name: "استرونجر ويز يو",
-      description: "Stronger With You - جاذبية لا تقاوم وحضور قوي.",
-      price: "1250 جنية",
+      id: 13,
+      name: "لاكوست وايت",
+      description: "Lacoste White - بساطة وأناقة تناسب كل يوم.",
+      price: "1100 جنية",
       size: "100 مل",
       image: "/img5_.jpg"
     },
     {
-      id: 6,
-      name: "مون بلو",
-      description: "Mont Blue - لمسة من الرقي والأناقة الكلاسيكية.",
-      price: "1200 جنية",
-      size: "100 مل",
-      image: "/img6_.jpg"
-    },
-    {
-      id: 7,
-      name: "بلو دي شانيل",
-      description: "Bleu De Chanel - الحرية في أسمى معانيها.",
-      price: "1400 جنية",
-      size: "100 مل",
-      image: "/img7_.jpg"
-    },
-    {
-      id: 8,
+      id: 14,
       name: "سلفر سنت",
       description: "Silver Scent - تباين مذهل بين البرودة والدفء.",
       price: "1150 جنية",
       size: "100 مل",
-      image: "/img8_.jpg"
-    },
-    {
-      id: 9,
-      name: "اسكلبشر",
-      description: "Sculptor - تحفة فنية منحوتة من الروائح العطرة.",
-      price: "1100 جنية",
-      size: "100 مل",
-      image: "/img9_.jpg"
-    },
-    {
-      id: 10,
-      name: "بكرات روج",
-      description: "Baccarat Rouge - سحر العنبر والزهور في تناغم فريد.",
-      price: "1500 جنية",
-      size: "100 مل",
-      image: "/img10_.jpg"
-    },
-    {
-      id: 11,
-      name: "لاوكست وايت",
-      description: "Lacoste White - بساطة وأناقة تناسب كل يوم.",
-      price: "1100 جنية",
-      size: "100 مل",
-      image: "/img1_.jpg"
-    },
-    {
-      id: 12,
-      name: "روز فانيليا",
-      description: "Rose Vanilla - حلاوة الفانيليا مع رقة الورد.",
-      price: "1250 جنية",
-      size: "100 مل",
-      image: "/img2_.jpg"
-    },
-    {
-      id: 13,
-      name: "عود ابيض",
-      description: "White Oud - نفحات شرقية فاخرة بلمسة عصرية.",
-      price: "1350 جنية",
-      size: "100 مل",
-      image: "/img3_.jpg"
-    },
-    {
-      id: 14,
-      name: "زارا جولد",
-      description: "Zara Gold - تألق وبريق يناسبك.",
-      price: "1000 جنية",
-      size: "100 مل",
-      image: "/img4_.jpg"
+      image: "/img6_.jpg"
     },
     {
       id: 15,
+      name: "اسكلبشر",
+      description: "Sculpture - تحفة فنية منحوتة من الروائح العطرة.",
+      price: "1100 جنية",
+      size: "100 مل",
+      image: "/img7_.jpg"
+    },
+    {
+      id: 16,
       name: "دانهل ديزاير",
       description: "Dunhill Desire - الرغبة والشغف في زجاجة عطر.",
       price: "1150 جنية",
       size: "100 مل",
-      image: "/img5_.jpg"
-    },
-    {
-      id: 16,
-      name: "سوفاج",
-      description: "Sauvage - نداء البرية وقوة الطبيعة.",
-      price: "1400 جنية",
-      size: "100 مل",
-      image: "/img6_.jpg"
+      image: "/img8_.jpg"
     }
   ]
-}
+};
